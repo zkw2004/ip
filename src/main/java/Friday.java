da7042acac4cf4a55beff1b9111ba3ce2d1d7e77
zkw2004 <zhangkaiwen2004@gmail.com>
2026-08-17 16:07:12 +0800

    Implemented echo in chatbot

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Friday chatbot application.
  */
 public class Friday {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String name = "Friday";
         String line = "____________________________________________________________";
         String banner = """
@@ -18,7 +21,19 @@ public class Friday {
         System.out.println("Hello! I'm " + name + ".");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")){
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(" " + command);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
