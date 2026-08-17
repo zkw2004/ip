4430535e20cb1585f460d4fd1fbe62543f5f8054
zkw2004 <zhangkaiwen2004@gmail.com>
2026-08-17 15:46:43 +0800

    Update the initial code to simply greet the user and exit.

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -3,12 +3,22 @@
  */
 public class Friday {
     public static void main(String[] args) {
-        String banner = " _____    _     _             \n"
-                + "|  ___| _(_) __| | __ _ _   _ \n"
-                + "| |_ | '__| |/ _` |/ _` | | | |\n"
-                + "|  _|| |  | | (_| | (_| | |_| |\n"
-                + "|_|  |_|  |_|\\__,_|\\__,_|\\__, |\n"
-                + "                         |___/ \n";
+        String name = "Friday";
+        String line = "____________________________________________________________";
+        String banner = """
+                 _____    _     _
+                |  ___| _(_) __| | __ _ _   _
+                | |_ | '__| |/ _` |/ _` | | | |
+                |  _|| |  | | (_| | (_| | |_| |
+                |_|  |_|  |_|\\__,_|\\__,_|\\__, |
+                                         |___/
+                """;
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
