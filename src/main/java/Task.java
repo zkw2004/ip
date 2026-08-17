777c11a2a607263f9a42bd567c6ce472cb62e82f
zkw2004 <zhangkaiwen2004@gmail.com>
2026-08-17 23:26:38 +0800

    Implemented task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+    public void markAsDone() {
+        this.isDone = true;
+    }
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+}
